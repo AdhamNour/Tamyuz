8255650105ab43337fd5a51c25c95f9c3d0af967
Adham Nour <adhamnourelwaffaa@gmail.com>
2021-11-16 17:57:34 +0200

    finished the welcome screen

--- lib/Welcome/WelcomeScreen.dart
+++ lib/Welcome/WelcomeScreen.dart
@@ -8,51 +8,77 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Column(
-                children: [
-                  const Icon(
-                    Icons.stacked_bar_chart,
-                    color: Colors.white,
-                    size: 128,
-                  ),
-                  Container(
-                    child: const Center(
-                      child: Text(
-                        'تمييز',
-                        style: TextStyle(color: Colors.white, fontSize: 48),
-                      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Column(
+                  children: [
+                    const Icon(
+                      Icons.stacked_bar_chart,
+                      color: Colors.white,
+                      size: 128,
                     ),
-                    // decoration: const BoxDecoration(color: Colors.amber),
-                    width: double.infinity,
-                    padding: const EdgeInsets.only(left: 20),
-                  ),
-                  const SizedBox(
-                    child: Center(
-                      child: Text(
-                        'هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
+                    Container(
+                      child: const Center(
+                        child: Text(
+                          'تمييز',
+                          style: TextStyle(color: Colors.white, fontSize: 48),
                         ),
-                        textAlign: TextAlign.center,
                       ),
+                      // decoration: const BoxDecoration(color: Colors.amber),
+                      width: double.infinity,
+                      padding: const EdgeInsets.only(left: 20),
                     ),
-                    // decoration: const BoxDecoration(color: Colors.amber),
-                    width: double.infinity,
-                    // padding: const EdgeInsets.only(),
-                  )
-                ],
-                mainAxisSize: MainAxisSize.min,
+                    const SizedBox(
+                      child: Center(
+                        child: Text(
+                          'هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      // decoration: const BoxDecoration(color: Colors.amber),
+                      width: double.infinity,
+                      // padding: const EdgeInsets.only(),
+                    )
+                  ],
+                  mainAxisSize: MainAxisSize.min,
+                ),
               ),
             ),
-          ),
-          ElevatedButton(onPressed: () {}, child: const Text('انشاء حساب')),
-          ElevatedButton(onPressed: () {}, child: const Text('نسجيل دخول')),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {},
+                  child: const Text('انشاء حساب',
+                      style: TextStyle(
+                          color: Color.fromARGB(200, 44, 62, 80), fontSize: 24)),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      minimumSize: MaterialStateProperty.all<Size>(
+                          const Size.fromHeight(50)))),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {},
+                  child: const Text('تسجيل الدخول',
+                      style: TextStyle(color: Colors.white, fontSize: 24)),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(200, 44, 62, 80)),
+                      minimumSize: MaterialStateProperty.all<Size>(
+                          const Size.fromHeight(50)))),
+            ),
+          ],
+        ),
       ),
       backgroundColor: Colors.blue,
     );
